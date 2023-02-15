3316f353840cfdb6c27e03bdb75f1ea94bb2f88c
osallahstevie <osallahstevie@gmail.com>
2023-02-15 13:34:29 +0300

    completed instance method lab

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+    #Class body
+    #Instance method definition
+    def bark
+        puts "Woof!"
+    end
+    def sit 
+        puts "The Dog is sitting"
+    end
+end
+rex = Dog.new
+rex.bark
+rex.sit
+
+fido = Dog.new
+fido.bark 
